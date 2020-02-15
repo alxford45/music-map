7d7a77039abc8b74dc0367ad69d85287c83dbde9
Alex <alxford45@gmail.com>
2020-02-15 00:29:43 -0600

    simplified index to implement webpack build

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
+import React from "react";
+import ReactDOM from "react-dom";
+// import "./index.css";
 
 import App from "./App";
 
-
-const client = new ApolloClient({
-    uri:
-        "https://g33nmiwnmnagnawqqw54sqvgl4.appsync-api.us-east-2.amazonaws.com/graphql",
-    headers: {
-        "X-Api-Key": "da2-4huensyzjndbnk5hnm5ebol23u"
-    }
-});
-
-ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>,
-    document.getElementById("root") as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
