9ae9cc77be893f9912394a880c01ff64e59ef3ec
Alex <alxford45@gmail.com>
2020-02-16 11:51:57 -0600

    re-added import to Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 // import './App.css';
 
-// import { Home } from "./pages/Home";
+import { Home } from "./pages/Home";
 
 const App: React.FC = () => {
-  return <div className="App">"hello world"</div>;
+  return <Home />;
 };
 
 export default App;
